9b4c834ca526fd5f3d3614c0704ef8defcb335e9
ruruping07 <ruruping07@gmail.com>
2021-03-15 22:12:25 +0900

    2021.03.15

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloProvider } from "@apollo/client";
+import { HelmetProvider } from "react-helmet-async";
 import { client } from "./apollo";
 import './styles/styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,3 +1,44 @@
 import React from "react";
 
-export const LoggedInRouter = () => <span>Logged In</span>;
+import { BrowserRouter as Router, Redirect, Route, Switch, } from "react-router-dom";
+import { Restaurants } from "../pages/client/restaurants";
+import { ConfirmEmail } from "../pages/user/confirm-email";
+import { EditProfile } from "../pages/user/edit-profile";
+import { Header } from "../components/header";
+import { useMe } from "../hooks/useMe";
+
+const ClientRoutes = [
+  <Route key={1} path="/" exact>
+    <Restaurants />
+  </Route>,
+  <Route key={2} path="/confirm" exact>
+    <ConfirmEmail />
+  </Route>,
+  <Route key={3} path="/edit-profile" exact>
+    <EditProfile />
+  </Route>,
+];
+
+export const LoggedInRouter = () => {
+  const { data, loading, error } = useMe();
+
+  console.log(data);
+
+  if (!data || loading || error) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <span className="font-medium text-xl tracking-wide">Loading..</span>
+      </div>
+    );
+  }
+
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        {data.me.role === "Client" && ClientRoutes}
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+};
